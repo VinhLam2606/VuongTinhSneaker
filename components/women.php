0143aad6c45a000bef7dc43e5e8ab7164c22dcb2
Vinh Lam <lamthevinh2606@gmail.com>
2025-03-31 12:56:49 +0700

    update women

--- components/women.php
+++ components/women.php
@@ -2,19 +2,16 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/footer.css">
-    <link rel="stylesheet" href="../style/navbar.css">
-    <link rel="stylesheet" href="../style/index.css">
-    <link rel="stylesheet" href="../style/men.css">
-
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../style/footer.css" />
+    <link rel="stylesheet" href="../style/navbar.css" />
+    <link rel="stylesheet" href="../style/men.css" />
     <script src="https://kit.fontawesome.com/683b4b40e3.js" crossorigin="anonymous"></script>
-    <link nes="" rel="icon" sizes="128x128" href="images/logo.png">
     <link
         href="https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed:wght@900&family=Oswald:wght@700&display=swap"
-        rel="stylesheet">
+        rel="stylesheet" />
     <title>Nike, Just Do It. Nike.com</title>
 </head>
 
@@ -27,7 +24,8 @@
             <?php include 'bottom.php'; ?>
         </div>
     </div>
-
+    
+    
     <div class="sort-container">
         <label for="sort">Sort by:</label>
         <select id="sort">
@@ -45,27 +43,27 @@
     <script>
         let currentPage = 1;
         let currentSort = 'default';
+        let currentSearch = new URLSearchParams(window.location.search).get('query') || '';
+        
+        function loadShoes(page = 1, sort = 'default', search = '') {
+            fetch(`load_shoes.php?page=${page}&sort=${sort}&gender=Women&query=${encodeURIComponent(search)}`)
+                .then(res => res.text())
+                .then(data => {
+                    const parser = new DOMParser();
+                    const html = parser.parseFromString(data, 'text/html');
+                    document.getElementById('shoes-container').innerHTML =
+                        html.getElementById('shoes-container').innerHTML;
+                    document.getElementById('pagination').innerHTML =
+                        html.getElementById('pagination').innerHTML;
+                }); 
+        }
 
-        function loadShoes(page = 1, sort = 'default') {
-    fetch(`load_shoes.php?page=${page}&sort=${sort}&gender=Women`)
-        .then(res => res.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const html = parser.parseFromString(data, 'text/html');
-            document.getElementById('shoes-container').innerHTML =
-                html.getElementById('shoes-container').innerHTML;
-            document.getElementById('pagination').innerHTML =
-                html.getElementById('pagination').innerHTML;
-        });
-}
-
-
-        window.onload = () => loadShoes();
+        window.onload = () => loadShoes(currentPage, currentSort, currentSearch);
 
         document.getElementById('sort').addEventListener('change', function() {
             currentSort = this.value;
             currentPage = 1;
-            loadShoes(currentPage, currentSort);
+            loadShoes(currentPage, currentSort, currentSearch);
             window.scrollTo({
                 top: 0,
                 behavior: 'smooth'
@@ -75,16 +73,15 @@
         document.addEventListener('click', function(e) {
             if (e.target.classList.contains('page-link')) {
                 e.preventDefault();
-                currentPage = parseInt(e.target.dataset.page);
-                loadShoes(currentPage, currentSort);
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
-                });
+                let page = parseInt(e.target.getAttribute('data-page')); // Lấy số trang từ `data-page`
+                loadShoes(page, currentSort, currentSearch);
+                window.scrollTo({ top: 0, behavior: 'smooth' });
             }
         });
+
     </script>
     <script src="../scripts/add_to_cart.js"></script>
+
 </body>
 
 </html>
